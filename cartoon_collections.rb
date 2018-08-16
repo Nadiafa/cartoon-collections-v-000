3ebeb1af55b31fd92ac4a58b6e93ce4dac7d107c
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-16 15:30:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(arr)
 end
 
 def summon_captain_planet(arr)
-  arr.each.upcase.push("!")
+  arr.map {|a| a.capitalize<<"!"}
 end
 
 def long_planeteer_calls# code an argument here
