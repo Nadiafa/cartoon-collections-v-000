4d8d737a581d26a0bc422012e334e898b7b20ea0
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-16 15:31:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(arr)
   arr.map {|a| a.capitalize<<"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  return true if arr.
 end
 
 def find_the_cheese# code an argument here
