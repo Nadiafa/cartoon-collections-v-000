b6171fc1a861e98a1dbee6b341488b04255eecdd
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-16 15:14:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+ i = 0 
+ while i < arr.length 
+  puts "#{i+1}. #{arr[i]}." 
+ end
 end
 
 def summon_captain_planet# code an argument here
